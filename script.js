30059520a834956749e5baefcc719e9baf9f0194
Sami Ul Haq <90406647+BabaKaana@users.noreply.github.com>
2024-09-03 14:28:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let button = document.querySelector("button");
+let h1 = document.querySelector("h1");
+
+let fname = prompt("What's your name?");
+h1.innerText = `Hello ${fname}!`;
+
+button.onclick = function()
+{
+   h1.innerHTML = "I LOVE YOU &#128536";
+   h1.style.color = "purple";
+   h1.style.fontSize = "120px";
+   h1.style.textAlign = "center";
+   h1.style.paddingTop = "40px";
+   document.querySelector("button").style.visibility = "hidden";
+
+};
